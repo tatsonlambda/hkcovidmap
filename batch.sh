f833d95c9ebbb696d91712601781e38741e36f6e
ymlai87416 <ymlai87416@gmail.com>
2021-01-11 13:22:29 +0800

    test

--- batch.sh
+++ batch.sh
@@ -1,7 +1,19 @@
 #! /bin/bash
 
+hour=$(date +%H)
+yesterday=$(date -d "yesterday" '+%Y-%m-%d')
+today=$(date +'%Y%m%d')
+
 pushd /home/ymlai/hkcovidmap
 
-python3  /home/ymlai/hkcovidmap/batch.py $(date +'%Y%m%d')
+if [ "$hour" -lt 8 -a "$hour" -ge 0 ]; then
+    echo "execute yesterday batch"
+    python3  /home/ymlai/hkcovidmap/batch.py $yesterday
+elif [ "$hour" -lt 24 -a "$hour" -ge 11 ]; then
+    echo "execute today batch"
+    python3  /home/ymlai/hkcovidmap/batch.py $today
+else
+    echo "skip, data not ready"
+fi
 
 popd
